b19a0276cb798fa81bc952c3d274897fccc65225
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2019-07-15 14:09:57 -0400

    BP: #05 Update both topic blogposts with correct PNG

---
+++ _site/auto/refs.el
@@ -0,0 +1,16 @@
+(TeX-add-style-hook
+ "refs"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "steele2004cauchyscwarzmaster_book"
+    "grahamknuthpatashnik1994concrete_book"
+    "korostelev2011asymptoticminimax_book"
+    "wainwright_highdimstats_book"
+    "candesstat300c_lec01"
+    "jordan2019artificial"
+    "xu2017information"
+    "pmlr-v51-russo16"
+    "dunn46_recor_linkag"
+    "rothman14_earth_carbon_cycle"))
+ :bibtex)
+
